07fb743a2ef0da85fa1373881f92fb748378fc64
ndrafke <nickdrafke@gmail.com>
2021-07-19 12:03:27 -0500

    navbar changes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,14 +5,14 @@ export default class Navigation extends Component{
     render(){
         return(
             <div>
-                <Navbar className="nav-color" fixed="top" bg="dark" expand="sm">
-  <Navbar.Brand href="#home"></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar className="nav-color" fixed="top" expand="sm">
+  <Navbar.Brand href="#home" className="order-sm-0 mx-auto order-1"></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" className="order-sm-1 order-0" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Stream</Nav.Link>
-      <Nav.Link href="#about">About</Nav.Link>
-      <Nav.Link href="#follow">Follow</Nav.Link>
+    <Nav className="ml-auto">
+      <Nav.Link href="#home"><h6 className="nav-text">Stream</h6></Nav.Link>
+      <Nav.Link href="#about"><h6 className="nav-text">About</h6></Nav.Link>
+      <Nav.Link href="#follow"><h6 className="nav-text">Follow</h6></Nav.Link>
       
       
     </Nav>
