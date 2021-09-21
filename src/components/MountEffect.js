68de8cfe58e3f62eac8bfe2b61878d3936e5395e
ndrafke <nickdrafke@gmail.com>
2021-09-21 14:14:37 -0500

    style updates

--- src/components/MountEffect.js
+++ src/components/MountEffect.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MountEffect() {
     return (
         <div id="mount">
-        <img src="https://i.imgur.com/krDbzgs.jpg" id="dpads-logo" alt="dpadsndice logo"></img>    
+        <img src="https://i.imgur.com/ZWTpRBo.jpg" id="dpads-logo" alt="dpadsndice logo"></img>    
         </div>
     )
 }
